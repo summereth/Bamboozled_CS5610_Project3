f37711fc72a7d433761de68f841e3098722489e0
summereth <liq.qian.l@gmail.com>
2025-02-27 17:38:10 -0800

    fix: restruct routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import { Outlet } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
+import CreateQuizScreen from "./screens/CreateQuizScreen.jsx";
 
-export default function App() {
+function App() {
   return (
-    <>
-      <Outlet />
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/create" element={<CreateQuizScreen />} />
+          {/* Additional routes can be added here */}
+        </Routes>
+      </main>
+    </Router>
   );
 }
+
+export default App;
