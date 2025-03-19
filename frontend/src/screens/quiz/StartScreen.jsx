0cd5f5311830a853370d595061b0102533425930
summereth <liq.qian.l@gmail.com>
2025-03-19 12:33:09 -0700

    feat: [client] add back button on StartScreen

--- frontend/src/screens/quiz/StartScreen.jsx
+++ frontend/src/screens/quiz/StartScreen.jsx
@@ -1,4 +1,6 @@
 import useQuiz from "../../contexts/useQuiz";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function StartScreen() {
   const { quizName, quizDescription, questionNum, dispatch } = useQuiz();
@@ -8,12 +10,17 @@ function StartScreen() {
       <h2>Welcome to {quizName}</h2>
       <h3>{quizDescription}</h3>
       <h3>{questionNum} questions to test your mastery!</h3>
-      <button
-        className="btn btn-ui"
-        onClick={() => dispatch({ type: "start" })}
-      >
-        Let's start!
-      </button>
+      <div className="d-flex justify-content-between">
+        <Button as={Link} to="/" className="btn btn-ui">
+          Back
+        </Button>
+        <button
+          className="btn btn-ui"
+          onClick={() => dispatch({ type: "start" })}
+        >
+          Let's start!
+        </button>
+      </div>
     </div>
   );
 }
